7d21be2e471b07ff16f0748b88b727b566b1594d
shotaronowhere <shawtarohgn@gmail.com>
2023-05-15 17:42:25 +0700

    chore(ui): remove hardhat wagmi plugin

--- ui/wagmi.config.ts
+++ ui/wagmi.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from '@wagmi/cli'
-import { react, hardhat } from '@wagmi/cli/plugins'
+import { react } from '@wagmi/cli/plugins'
+import { erc20ABI } from 'wagmi'
 
 export default defineConfig({
   out: 'lib/blockchain.ts',
-  plugins: [
-    hardhat({
-      project: '../contracts',
-    }),
+  contracts: [
+    {
+      name: 'erc20',
+      abi: erc20ABI,
+    },
   ],
+  plugins: [react()],
 })
