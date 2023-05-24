a739409fc7b9f9a256806f1a0699b470170f8769
shotaronowhere <shawtarohgn@gmail.com>
2023-05-24 16:04:36 +0700

    feat(ui): big update

--- ui/app/goerli/page.tsx
+++ ui/app/goerli/page.tsx
@@ -7,12 +7,13 @@ import { BranchIsWalletConnected } from '@/components/shared/branch-is-wallet-co
 import { Button } from '@/components/ui/button'
 import { FADE_DOWN_ANIMATION_VARIANTS } from '@/config/design'
 import { publicProvider } from 'wagmi/providers/public'
-import {goerli, arbitrumGoerli, gnosisChiado} from 'wagmi/chains' 
+import {goerli, arbitrumGoerli} from 'wagmi/chains' 
 import { readContract, configureChains, watchContractEvent, createClient } from '@wagmi/core'
 import { BigNumber } from 'ethers'
+import { Tooltip } from '@/components/ui/tooltip'
 const abi = require('../../assets/VeaOutboxArbToEthDevnet.json').abi
 const { provider, webSocketProvider } = configureChains(
-  [arbitrumGoerli, gnosisChiado],
+  [goerli, arbitrumGoerli],
   [publicProvider()],
 )
 
@@ -36,30 +37,38 @@ import Link from 'next/link'
 export default function Home() {
   const [light, setLight] = useState(false)
   const [hitSwitch, setHitSwitch] = useState("loading...")
+  const [snapshotTaken, setSnapshotTaken] = useState("loading...")
   const [claimVerified, setClaimVerified] = useState("loading...")
+  const [relayedTx, setRelayedTx] = useState("loading...")
   const [time, setTime] = useState(0)
   const [veaBridgeConfirmation, setveaBridgeConfirmation] = useState(false)
   const [switchEpoch, setSwitchEpoch] = useState(0)
   var condition = light ? 'on' : 'off'
   var relayed = light ? '' : 'none'
   var switchHit = hitSwitch != "loading..."
+  var snapshotTakenBool = snapshotTaken != "loading..."
+  var claimVerifiedBool = claimVerified != "loading..."
+  var relayedTxBool = relayedTx != "loading..."
   var tableDisplay = light || switchHit ? '' : 'none'
   var timerDisplay = switchHit && !light && !veaBridgeConfirmation ? '' : 'none'
   var bridgeSuccess = veaBridgeConfirmation || light ? '' : 'none'
   const count = useRef(0)
-  var claimVerifiedBool = claimVerified != "loading..."
 
   const account = useAccount({
     onDisconnect() {
       setLight(false);
       setHitSwitch("loading...");
+      setSwitchEpoch(0)
+      setSnapshotTaken("loading...");
+      setClaimVerified("loading...");
+      setRelayedTx("loading...");
       setveaBridgeConfirmation(false);
       setTime(0)
     },
 
     async onConnect({ address, connector, isReconnected }) {
       const lightBulbIsOn = await readContract({
-        address: '0xb525F403026DfE08ae0200834cBCAa27a56FD6A3',
+        address: '0xb4042dF3F7E8A0d9EE2CD65FDc41963e1F31D608',
         abi: [
           {
             inputs: [
@@ -82,12 +91,12 @@ export default function Home() {
           },
         ],
         functionName: 'lightBulbIsOn',
-        chainId: 10200,
+        chainId: 5,
         args: [address!],
       })
 
       const latestVerifiedEpoch = (await readContract({
-        address: '0xdFd7aDEb43d46FA3f16FB3e27F7fe85c3f5BD89D',
+        address: '0x9235A379950B9f01fb3e2961C06912A96DCcef0e',
         abi: [
           {
             inputs:[],
@@ -97,7 +106,7 @@ export default function Home() {
             type:"function"}
         ],
         functionName: 'latestVerifiedEpoch',
-        chainId: 10200,
+        chainId: 5,
       }) as BigNumber).toNumber()
 
       console.log('lightBulbIsOn', lightBulbIsOn)
@@ -108,7 +117,7 @@ export default function Home() {
       const lightBulbToggles: any = await request(
         'https://api.thegraph.com/subgraphs/name/shotaronowhere/vea-lightbulb',
         `{
-          lightBulbToggleds(first: 1, where: {lightBulbOwner: "${address}", chainId: 10200}, orderBy: blockTimestamp, orderDirection: asc) {
+          lightBulbToggleds(first: 1, where: {lightBulbOwner: "${address}", chainId: 5}, orderBy: blockTimestamp, orderDirection: asc) {
             messageId
             lightBulbOwner
             transactionHash
@@ -131,31 +140,56 @@ export default function Home() {
           console.log('msg epoch', epoch);
           setveaBridgeConfirmation(true)
           console.log('latestVerifiedEpoch', latestVerifiedEpoch)
-          /*
-          const bridgeTxn = await request(
-            'https://thegraph.com/hosted-service/subgraph/alcercu/veascan-outbox-goerli',
+          const bridgeTxn: any = await request(
+            'https://api.thegraph.com/subgraphs/name/shotaronowhere/veascan-outbox-goerli',
             `{
-              claims(first: 1, orderBy: epoch, where: {epoch_gte: 3}) {
+  claims(first: 1, where:{epoch_gte: ${epoch}}, orderBy: epoch, orderDirection: desc) {
+    txHash
+  }
+    messages(first: 5, where: {id: ${msgid}}) {
+      id
+      txHash
+    }
+}`
+          )
+          if (bridgeTxn.claims.length > 0) {
+            console.log(bridgeTxn.claims[0].txHash)
+            setSnapshotTaken(bridgeTxn.claims[0].txHash);
+          } else {
+            console.log('no bridge txn')
+          }
+          if(bridgeTxn.messages.length > 0){
+            console.log(bridgeTxn.messages[0].txHash)
+            setRelayedTx(bridgeTxn.messages[0].txHash);
+          } else {
+            console.log('no relayed txn')
+          }
+
+          const bridgeTxn2: any = await request(
+            'https://api.thegraph.com/subgraphs/name/shotaronowhere/veascan-inbox-arbitrumgoerli-g',
+            `          {
+              snapshots(first: 1, where: {epoch_gte: ${epoch}}, orderBy: epoch, orderDirection: asc) {
                 txHash
               }
             }`
-          )*/
-        }
-        const bridgeTxn2: any = await request(
-          'https://api.thegraph.com/subgraphs/name/shotaronowhere/veascan-inbox-arbitrumgoerli-g',
-          `          {
-            snapshots(first: 1, where: {epoch_gte: ${epoch}}, orderBy: epoch, orderDirection: asc) {
-              txHash
-            }
-          }`
-        )
-        if (bridgeTxn2.snapshots.length > 0) {
-          console.log(bridgeTxn2.snapshots[0].txHash)
-          setClaimVerified(bridgeTxn2.snapshots[0].txHash);
-        } else {
-          console.log('no bridge txn')
-        }
+          )
+          if (bridgeTxn2.snapshots.length > 0) {
+            console.log(bridgeTxn2.snapshots[0].txHash)
+            setClaimVerified(bridgeTxn2.snapshots[0].txHash);
+          } else {
+            console.log('no bridge txn')
+          }
 
+          /*
+{
+  messages(first: 5, where: {id: 1}) {
+    id
+    txHash
+  }
+}
+          */
+
+        }
         const estimatedBridgeTimeComplete = Math.ceil((switchTime / 1800))*1800 + 60
         const estimatedBridgeTime = Math.max(estimatedBridgeTimeComplete - Math.floor(Date.now()/1000), 0)
         setTime(estimatedBridgeTime)
@@ -163,8 +197,10 @@ export default function Home() {
         console.log('time set', estimatedBridgeTime)
       } else {
         setHitSwitch("loading...")
+        setSnapshotTaken("loading...")
         setSwitchEpoch(0)
         setClaimVerified("loading...")
+        setRelayedTx("loading...")
         setveaBridgeConfirmation(false);
         setTime(0)
       }
@@ -173,10 +209,10 @@ export default function Home() {
 
   const unwatch = watchContractEvent(
     {
-      address: '0xdFd7aDEb43d46FA3f16FB3e27F7fe85c3f5BD89D',
+      address: '0x9235A379950B9f01fb3e2961C06912A96DCcef0e',
       abi: abi,
       eventName: 'Verified',
-      chainId: 10200,
+      chainId: 5,
       once: true,
     },
     (epoch) => {
@@ -200,7 +236,7 @@ export default function Home() {
   if(time == 0){
     console.log('yoyoyoyoyoyo')
     const { data, isError, isLoading } = useContractRead({
-      address: '0xdFd7aDEb43d46FA3f16FB3e27F7fe85c3f5BD89D',
+      address: '0x74F0E300aA91F207E4DF6388a73ba458D7Dc3Cc5',
       abi: [
         {
           inputs:[],
@@ -210,7 +246,7 @@ export default function Home() {
           type:"function"}
       ],
       functionName: 'latestVerifiedEpoch',
-      chainId: 10200,
+      chainId: 5,
       onSuccess(data) {
         console.log('Success', data)
       },
@@ -234,14 +270,16 @@ export default function Home() {
   
 	const handleConnectorUpdate = async ({account, chain}: ConnectorData) => {
     setHitSwitch("loading...")
-    setTime(0)
+    setSnapshotTaken("loading...")
     setSwitchEpoch(0)
-    setveaBridgeConfirmation(false);
     setClaimVerified("loading...")
-
+    setRelayedTx("loading...")
+    setTime(0)
+    setveaBridgeConfirmation(false);
+    
     if (account) {
       const lightBulbIsOn = await readContract({
-        address: '0xb525F403026DfE08ae0200834cBCAa27a56FD6A3',
+        address: '0xb4042dF3F7E8A0d9EE2CD65FDc41963e1F31D608',
         abi: [
           {
             inputs: [
@@ -264,7 +302,7 @@ export default function Home() {
           },
         ],
         functionName: 'lightBulbIsOn',
-        chainId: 10200,
+        chainId: 5,
         args: [account!],
       })
       console.log('lightBulbIsOn', lightBulbIsOn)
@@ -275,7 +313,7 @@ export default function Home() {
       const lightBulbToggles: any = await request(
         'https://api.thegraph.com/subgraphs/name/shotaronowhere/vea-lightbulb',
         `{
-          lightBulbToggleds(first: 1, where: {lightBulbOwner: "${account}", chainId: 10200}, orderBy: blockTimestamp, orderDirection: asc) {
+          lightBulbToggleds(first: 1, where: {lightBulbOwner: "${account}", chainId: 5}, orderBy: blockTimestamp, orderDirection: asc) {
             messageId
             lightBulbOwner
             blockTimestamp
@@ -293,6 +331,32 @@ export default function Home() {
         setTime(estimatedBridgeTime)
         setHitSwitch(lightBulbToggles.lightBulbToggleds[0].transactionHash)
         console.log('time set', estimatedBridgeTime)
+        const msgid = lightBulbToggles.lightBulbToggleds[0].messageId
+        setSwitchEpoch(Math.floor(switchTime/1800))
+        const bridgeTxn: any = await request(
+          'https://api.thegraph.com/subgraphs/name/shotaronowhere/veascan-outbox-goerli',
+          `{
+claims(first: 1, where:{epoch_gte: ${Math.floor(switchTime/1800)}}, orderBy: epoch, orderDirection: desc) {
+  txHash
+}
+  messages(first: 5, where: {id: ${msgid}}) {
+    id
+    txHash
+  }
+}`
+        )
+        if (bridgeTxn.claims.length > 0) {
+          console.log(bridgeTxn.claims[0].txHash)
+          setSnapshotTaken(bridgeTxn.claims[0].txHash);
+        } else {
+          console.log('no bridge txn')
+        }
+        if(bridgeTxn.messages.length > 0){
+          console.log(bridgeTxn.messages[0].txHash)
+          setRelayedTx(bridgeTxn.messages[0].txHash);
+        } else {
+          console.log('no relayed txn')
+        }
         const bridgeTxn2: any = await request(
           'https://api.thegraph.com/subgraphs/name/shotaronowhere/veascan-inbox-arbitrumgoerli-g',
           `          {
@@ -307,7 +371,6 @@ export default function Home() {
         } else {
           console.log('no bridge txn')
         }
-        
       } 
     } else if (chain) {
       console.log('new chain', chain)
@@ -324,7 +387,7 @@ export default function Home() {
   , [light, account.connector, time])
 
   const { config, error } = usePrepareContractWrite({
-    address: '0xcDCB9CebbD0182408bfcD910D9df216F760D376C',
+    address: '0x2598934001f1B466Bc2bcCE79943CEcB859A2551',
     abi: [
       {
         inputs: [],
@@ -357,7 +420,7 @@ export default function Home() {
         viewport={{ once: true }}>
         <div className={`flex-center ${condition} body flex h-full w-full`}>
           <div className="light" style={{ margin: '10px 0' }}>
-            <div className="gnosis"></div>
+            <div className="eth"></div>
             <div className="wire"></div>
             <div className="bulb">
               <span></span>
@@ -375,7 +438,7 @@ export default function Home() {
     {/* head */}
     <thead>
     <tr>
-      <th>Step</th>
+    <th>Step</th>
       <th>Chain</th>
       <th>Txn</th>
       <th>Status</th>
@@ -391,12 +454,13 @@ export default function Home() {
       </tr>
       {/* row 2 */}
       <tr className="hover">
-      <th>Vea 1 / 2 &nbsp; 🌉</th>
+        <th>Vea 1 / 2 &nbsp; 🌉</th>
         <td><img src='/icons/NetworkArbitrumTest.svg'></img></td>
         <td><div style={{display: bridgeSuccess}}><span style={{display: "flex"}}>{claimVerifiedBool? claimVerified.substring(0,5)+'...'+claimVerified.substring(claimVerified.length-4,claimVerified.length-1) : claimVerified}&nbsp;<a target="_blank" href={"https://goerli.arbiscan.io/tx/"+claimVerified}><FiExternalLink/></a></span></div></td>
         <td><div style={{display: timerDisplay}}>
                 <div className="countdown font-mono">
-                  ~<span id="minute" style={{ '--value': Math.floor((time - Math.floor(time / 3600) * 3600) / 60) } as React.CSSProperties}></span>:
+                  <span id="hour" style={{ "--value": 0 } as React.CSSProperties}></span>:
+                  <span id="minute" style={{ '--value': Math.floor((time - Math.floor(time / 3600) * 3600) / 60) } as React.CSSProperties}></span>:
                   <span
                     id="second"
                     style={{
@@ -409,8 +473,8 @@ export default function Home() {
       </tr>
       <tr className="hover">
         <th>Vea 2 / 2  &nbsp;  🌉</th>
-        <td><img src='/icons/NetworkGnosis.svg'></img></td>
-        <td><div style={{display: bridgeSuccess}}><span style={{display: "flex"}}>veascan<a target="_blank" href="https://veascan.io/"><FiExternalLink/></a></span></div></td>
+        <td><img src='/icons/NetworkEthereum.svg'></img></td>
+        <td><div style={{display: bridgeSuccess}}><span style={{display: "flex"}}>{snapshotTakenBool? snapshotTaken.substring(0,5)+'...'+snapshotTaken.substring(snapshotTaken.length-4,snapshotTaken.length-1) : snapshotTaken}&nbsp;<a target="_blank" href={"https://goerli.etherscan.io/tx/"+snapshotTaken}><FiExternalLink/></a></span></div></td>
         <td>
             <div style={{display: bridgeSuccess}}>✅</div>
           </td>
@@ -418,8 +482,8 @@ export default function Home() {
       {/* row 3 */}
       <tr className="hover">
       <th>Lightbulb 💡</th>
-        <td><img src='/icons/NetworkGnosis.svg'></img></td>
-        <td><div style={{display: relayed}}><span style={{display: "flex"}}>veascan<a target="_blank" href="https://veascan.io/"><FiExternalLink/></a></span></div></td>
+        <td><img src='/icons/NetworkEthereum.svg'></img></td>
+        <td><div style={{display: relayed}}><span style={{display: "flex"}}>{relayedTxBool? relayedTx.substring(0,5)+'...'+relayedTx.substring(relayedTx.length-4,relayedTx.length-1) : relayedTx}&nbsp;<a target="_blank" href={"https://goerli.etherscan.io/tx/"+relayedTx}><FiExternalLink/></a></span></div></td>
         <td><div style={{display: relayed}}>✅</div>
 </td>
       </tr>
--- ui/components/layout/navigation-menu-general.tsx
+++ ui/components/layout/navigation-menu-general.tsx
@@ -27,17 +27,14 @@ export function NavigationMenuGeneral() {
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[600px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
-                  <div className="relative flex h-full w-full select-none flex-col justify-end overflow-hidden rounded-md bg-gradient-to-b from-purple-500 to-purple-700 p-6 no-underline outline-none focus:shadow-md">
+                  <div className="relative flex h-full w-full select-none flex-col justify-end overflow-hidden rounded-md bg-gradient-to-b from-blue-800 to-blue-900 p-6 no-underline outline-none focus:shadow-md">
                     <div className="absolute top-10 right-0 z-0 h-48 w-48 bg-[url('https://em-content.zobj.net/thumbs/240/twitter/322/high-voltage_26a1.png')] bg-cover opacity-20" />
                     <div className="z-10">
                       <h3 className="z-10 mb-2 mt-4 text-lg font-medium text-white">
-                        <span className="text-4xl">🌉</span>
-                        <br />
-                        Build with Vea!
+                        <span className="text-4xl">Build with Vea! 🌉</span>
                       </h3>
                       <p className="mb-3 text-sm leading-tight text-white/90">
-                        The Vea Lightbulb a cross-chain demo. Vea bridges messages from the switch and to the bulb which are deployed on different
-                        chains.
+                      The 🎚 and 💡 live on different chains, and communicate with Vea.
                       </p>
                       <p className="text-sm font-bold leading-tight text-white/90">#VEA</p>
                     </div>
@@ -47,16 +44,16 @@ export function NavigationMenuGeneral() {
               <li className="flex flex-col gap-4">
                 <LinkComponent href="/">
                   <div className="bg-card-with-hover card">
-                    <h3 className="text-lg font-bold">🎛️ Chiado</h3>
+                  <span style={{display: "flex"}}><img src='/icons/NetworkArbitrumTest.svg'></img><svg xmlns="http://www.w3.org/2000/svg" width="12" height="24" fill-rule="evenodd" clip-rule="evenodd"><path d=""/></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill-rule="evenodd" clip-rule="evenodd"><path fill="#FFFFFF" d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="12" height="24" fill-rule="evenodd" clip-rule="evenodd"><path d=""/></svg><img src='/icons/NetworkGnosis.svg'></img></span>
                     <div className="my-2" />
-                    <p className="text-xs">Chiado.</p>
+                    <p className="text-xs"><h3 className="text-xxl font-bold">Chiado. Send messages from Arbitrum to Gnosis on testnet (chiado).</h3></p>
                   </div>
                 </LinkComponent>
-                <LinkComponent href="/">
-                  <div className="bg-card-with-hover card">
-                    <h3 className="text-lg font-bold">Goerli</h3>
+                <LinkComponent href="/goerli">
+                <div className="bg-card-with-hover card">
+                  <span style={{display: "flex"}}><img src='/icons/NetworkArbitrumTest.svg'></img><svg xmlns="http://www.w3.org/2000/svg" width="12" height="24" fill-rule="evenodd" clip-rule="evenodd"><path d=""/></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill-rule="evenodd" clip-rule="evenodd"><path fill="#FFFFFF" d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="12" height="24" fill-rule="evenodd" clip-rule="evenodd"><path d=""/></svg><img src='/icons/NetworkEthereum.svg'></img></span>
                     <div className="my-2" />
-                    <p className="text-xs">Goerli.</p>
+                    <p className="text-xs"><h3 className="text-xxl font-bold">Goerli. Send messages from Arbitrum to Goerli on testnet (goerli).</h3></p>
                   </div>
                 </LinkComponent>
               </li>
